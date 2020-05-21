7c828847a331a01396f5665a6530a6f925e566be
Matti Eiden <matti.eiden@bitfactor.fi>
2020-05-22 00:12:25 +0300

    Create docs

---
+++ docs/precache-manifest.98745fc3da3dd5bc0e6c61e4e6d72126.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c7626d41f49a3809d715e8e9838f81",
+    "url": "/twi-baud-calculator/index.html"
+  },
+  {
+    "revision": "7a18316e613c485fede9",
+    "url": "/twi-baud-calculator/static/css/main.7b087aca.chunk.css"
+  },
+  {
+    "revision": "c83283b691457edeec79",
+    "url": "/twi-baud-calculator/static/js/2.5dfea3cf.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/twi-baud-calculator/static/js/2.5dfea3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a18316e613c485fede9",
+    "url": "/twi-baud-calculator/static/js/main.455878de.chunk.js"
+  },
+  {
+    "revision": "c345114cb730f710800a",
+    "url": "/twi-baud-calculator/static/js/runtime-main.d738e60d.js"
+  }
+]);
